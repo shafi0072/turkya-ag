23f1677235a8df84d22bf4110b6225adabd0a1ef
safidark <safi29317@gmail.com>
2021-11-01 23:45:58 +0600

    shafi

--- src/Componend/Home/Header/Header.jsx
+++ src/Componend/Home/Header/Header.jsx
@@ -6,6 +6,14 @@ const Header = () => {
     return (
         <div className="headerBackground">
             <Navbar/>
+            <div className="container">
+                <div className="d-flex justify-content-center">
+                    <div className="headingMargin">
+                        <h1 className="text-light headingText">Bienvenue chez The Shark 3D</h1>
+                        <h2 className="text-light paragraphHeading">Services de conception et de fabrication additive Impression<br/>3D industrielle</h2>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
