5b2f79b84a176c4c68cd9e9ffa66849c1aac9149
Raktim02 <you@example.com>
2021-11-01 21:45:26 +0600

    raktim

--- src/Componend/Home/Navbar/Navbar.jsx
+++ src/Componend/Home/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import './Navbar.scss';
 
-const Header = () => {
+const Navbar = () => {
     return (
         <div>
             
@@ -8,4 +9,4 @@ const Header = () => {
     );
 };
 
-export default Header;
+export default Navbar;
