66fbc27c50d02ecbf525205acc0b917b58613f7d
Raktim02 <you@example.com>
2021-11-02 11:36:59 +0600

    raktim

---
+++ src/Componend/Home/Exemples/Exemples.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Examples.scss';
+
+const Exemples = () => {
+    return (
+        <div>
+            <div>
+                hello
+            </div>
+        </div>
+    );
+};
+
+export default Exemples;
--- src/Componend/Home/Index/Index.jsx
+++ src/Componend/Home/Index/Index.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-
 import Besion from '../Besion/Besion';
-
-
 import Header from '../Header/Header';
 import Service from '../Service/Service';
 import WhoWeAre from '../WhoWeAre/WhoWeAre';
+import Exemples from '../Exemples/Exemples';
 
 
 
@@ -13,14 +11,10 @@ const Index = () => {
     return (
         <div>
             <Header/>
-
-
-
-
             <WhoWeAre/>
             <Service/>
             <Besion/>
-
+            <Exemples/>
         </div>
     );
 };
